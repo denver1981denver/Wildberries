fbdc401462d9a019d8054d89ce78f5b2f93a2c31
denver1981denver <84741346+denver1981denver@users.noreply.github.com>
2021-11-19 11:35:26 +0300

    Add files via upload

---
+++ js/search.js
@@ -0,0 +1,16 @@
+const search = () => {
+	const input = document.querySelector('.search-block > input'),
+		searchBtn = document.querySelector('.search-block > button');
+
+	// input.addEventListener('input', (event) => {
+	// 	console.log(event.target.value);
+	// })
+
+	searchBtn.addEventListener('click', (event) => {
+		console.log(input.value);
+	})
+	// написать обработчик события click на кнопке и по этому события выводить input.value в консоль
+};
+
+search();
+
