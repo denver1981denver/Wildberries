2de7b1ceb406bbecdd7e708e54c4534d6c328d2c
Denis <dennik1981@Gmail.com>
2021-11-19 10:25:49 +0300

    test

---
+++ js/search.js
@@ -0,0 +1,13 @@
+const search = () => {
+	const input = document.querySelector('.search-block > input'),
+		searchBtn = document.querySelector('.search-block > button');
+
+	input.addEventListener('input', (event) => {
+		console.log(event.target.value);
+	})
+
+	
+};
+
+search();
+
