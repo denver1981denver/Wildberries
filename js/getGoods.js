16488b34eed58a83bf1fe47d0f5739d928dc7f85
Denis <dennik1981@Gmail.com>
2021-11-20 13:38:19 +0300

    feature[js/getGoods.js]:adding new functionality

--- js/getGoods.js
+++ js/getGoods.js
@@ -6,18 +6,28 @@ const getGoods = () => {
    //    })
 	const links = document.querySelectorAll('.navigation-link');
 	
-	links.forEach(function (item) {
-		console.log(item);
-	})
-
   const getData = () => {
     fetch('https://willberries-6355c-default-rtdb.europe-west1.firebasedatabase.app/db.json')
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        localStorage.setItem('data', JSON.stringify(data)); // При получении данных от сервера сохранять data в localStorage.
       });
   }
 
+	links.forEach((link) => {
+		link.addEventListener('click', (event) => {
+			event.preventDefault();
+			getData();
+		})
+	})
+
+	// localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4, 5]));
+	// // console.log(goods);
+	// const goods = JSON.parse(localStorage.getItem('goods'));
+	// console.log(localStorage);
+	// localStorage.removeItem('goods');
+	// console.log(localStorage);
+	
 };
 
 getGoods();
